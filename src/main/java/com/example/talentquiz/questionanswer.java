fea7ecbcef35982acb7bdb07ee92c566587ace2f
JAYAPRIYA S <116139415+JAYAPRIYA007@users.noreply.github.com>
2024-02-19 12:50:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/talentquiz/questionanswer.java
@@ -0,0 +1,24 @@
+package com.example.talentquiz;
+
+public class questionanswer {
+
+        public static String question[] = {
+                "which company owns the android ?",
+                "which one is not the programming language ?",
+                "Android is ?"
+
+        };
+        public static String Choices[][] = {
+                {"apple", "google", "amazon", "samsung"},
+                {"java", "kotlin", "notepad", "python"},
+                {"an operating system ", "a web server  ", "a web browser ", "none of the above "}
+
+        };
+        public static String correctAnswers[] = {
+                "google",
+                "notepad",
+                "an operating system"
+
+        };
+    }
+
